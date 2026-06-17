d519f7a8c7041d89284509f6fc6787e9fff225d5
Aanish-Abhishek <aanishabhishek66@gmail.com>
2026-06-18 00:24:23 +0530

    chatbot setup

---
+++ backend.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI 
+from langgraph.checkpoint.memory import MemorySaver
+from typing import List
+from langgraph.graph.message import add_messages
+
+
+class ChatState(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+llm = ChatOpenAI()
+
+def chat_node(state: ChatState):
+   
+    #take user query from state
+    messages = state['messages']
+
+    #send to llm
+    response = llm.invoke(messages)
+
+    #response store state
+    return {'messages': [response]}
+
+check_pointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=check_pointer)
+
