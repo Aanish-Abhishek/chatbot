20f636de7443cca8e88d200c577d390c69e3d2e0
Aanish-Abhishek <aanishabhishek66@gmail.com>
2026-06-21 23:53:06 +0530

    setup local sqlite db

--- backend.py
+++ backend.py
@@ -2,10 +2,11 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI 
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from typing import List
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -25,7 +26,18 @@ def chat_node(state: ChatState):
     #response store state
     return {'messages': [response]}
 
-check_pointer = MemorySaver()
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in check_pointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+#using same db in different thread
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
+check_pointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 
@@ -35,4 +47,3 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=check_pointer)
-
